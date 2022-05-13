7df8e5f6e92c1e5f2820967d5cbb454b09fc9fe9
혁민 남 <avalatte30@imagoworks.ai>
2022-05-13 11:06:31 +0900

    Plus test 함수 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const plus = (a: number, b: number) => {
+    return a + b;
+  };
+
+  const a = plus(3, 5);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {a}Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn Reac
